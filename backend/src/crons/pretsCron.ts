c3997f744f8d7c219f3d4944a834b43e6b53ce50
asmabellil <asma.bellil77@gmail.com>
2024-09-03 14:25:04 +0100

    adjust_cron

--- backend/src/crons/pretsCron.ts
+++ backend/src/crons/pretsCron.ts
@@ -1,29 +1,35 @@
-import { Pret } from 'db';
+import { Echance, Pret } from 'db';
 import cron from 'node-cron';
-import { Op } from 'sequelize';
 
 // This function will update the moneyStatus
 async function updateMoneyStatus() {
-  const prets = await Pret.findAll({
+  const prets: any = await Pret.findAll({
     where: {
       moneyStatus: false,
     },
+    include: [{
+      model: Echance,
+      order: [['dateEcheance', 'DESC']], // Order by dateEcheance to get the last one
+    }],
   });
 
   const today = new Date();
 
   for (const pret of prets) {
-    const expectedDate = new Date(pret.dateEcheance);
-    expectedDate.setMonth(expectedDate.getMonth() + pret.echanceNumber -1 );
+    if (pret.Echances.length > 0) {
+      const lastEchance = pret.Echances[0]; // The first one after sorting is the last `Echance`
+      const expectedDate = new Date(lastEchance.dateEchance);
+      
 
-    if (today > expectedDate) {
-      await pret.update({ moneyStatus: true });
+      if (today > expectedDate) {
+        await pret.update({ moneyStatus: true });
+      }
     }
   }
 }
 
 // Schedule the cron job to run daily at midnight
-cron.schedule('0 0 * * *', async () => {
+cron.schedule('0 8 * * *', async () => {
   console.log('Running daily cron job to update moneyStatus');
   await updateMoneyStatus();
 });
